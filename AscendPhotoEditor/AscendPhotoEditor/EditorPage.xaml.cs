8da3206c18b4b340d340bc6f45b8db172a00a0fb
caleb <calebplant@aggienetwork.com>
2019-07-02 14:39:12 -0500

    Added metadata ListView on EditorPage

--- AscendPhotoEditor/AscendPhotoEditor/EditorPage.xaml.cs
+++ AscendPhotoEditor/AscendPhotoEditor/EditorPage.xaml.cs
@@ -14,12 +14,40 @@ namespace AscendPhotoEditor
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EditorPage : ContentPage
     {
-        private string imagePath { get; set;}
         public EditorPage(string imagePath)
         {
             InitializeComponent();
 
+            // Set image
             selectedImage.Source = imagePath;
+
+            // Get Image metadata
+            //List<MetadataEntry> metadataList = new List<MetadataEntry>();
+
+            //Dummy data for testing
+            List<MetadataEntry> metadataList = new List<MetadataEntry>();
+            metadataList.Add(new MetadataEntry("one", "111"));
+            metadataList.Add(new MetadataEntry("two", "222"));
+            metadataList.Add(new MetadataEntry("three", "333"));
+
+            // Set metadata list
+
+            metadataListView.ItemsSource = metadataList;
+            metadataListView.ItemTemplate = new DataTemplate(typeof(EntryCell));
+            metadataListView.ItemTemplate.SetBinding(EntryCell.LabelProperty, "ItemType");
+            metadataListView.ItemTemplate.SetBinding(EntryCell.TextProperty, "ItemDetails");
+        }
+    }
+
+    public class MetadataEntry
+    {
+        public string ItemType { get; set; }
+        public string ItemDetails { get; set; }
+
+        public MetadataEntry(string itemType, string itemDetails)
+        {
+            this.ItemType = itemType;
+            this.ItemDetails = itemDetails;
         }
     }
 }
